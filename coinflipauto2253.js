880362edc672b292ef6929e5adde18c6334eac59
pechipao <49608898+pechipao@users.noreply.github.com>
2019-05-27 18:56:25 +0200

    Add files via upload

---
+++ coinflipauto2253.js
@@ -0,0 +1,99 @@
+var input = document.getElementById("user_bet_value");
+var boton = document.getElementsByClassName("coinflip_container_coin_play coinflip_start bet_ready")[0];
+var texto = document.getElementsByClassName("coinflip_information aa")[0];
+var nivel = 0;
+
+function automatizar() {
+    setTimeout(function() {
+        boton.click();
+    },500)
+    setTimeout(function() {
+        if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+            document.getElementById("user_bet_value").value = "0.06";
+            setTimeout(function() {
+                if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                    document.getElementById("user_bet_value").value = "0.11";
+                    boton.click();
+                    setTimeout(function() {
+                        if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                            document.getElementById("user_bet_value").value = "0.22";
+                            boton.click();
+                            setTimeout(function() {
+                                if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                                    document.getElementById("user_bet_value").value = "0.44";
+                                    boton.click();
+                                    setTimeout(function() {
+                                        if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                                            document.getElementById("user_bet_value").value = "0.88";
+                                            boton.click();
+                                            setTimeout(function() {
+                                                if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                                                    document.getElementById("user_bet_value").value = "1.76";
+                                                    boton.click();
+                                                    setTimeout(function() {
+                                                        if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                                                            document.getElementById("user_bet_value").value = "3.52";
+                                                            boton.click();
+                                                            setTimeout(function() {
+                                                                if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                                                                    document.getElementById("user_bet_value").value = "7";
+                                                                    boton.click();
+                                                                    setTimeout(function() {
+                                                                        if (texto.innerHTML == "Unfortunately you lose. Try again.") {
+                                                                            document.getElementById("user_bet_value").value = "0.05";
+                                                                            boton.click();
+                                                                        }
+                                                                        else {
+                                                                            document.getElementById("user_bet_value").value = "0.05"
+                                                                            automatizar();
+                                                                        }
+                                                                    },3000)
+                                                                }
+                                                                else {
+                                                                    document.getElementById("user_bet_value").value = "0.05"
+                                                                    automatizar();
+                                                                }
+                                                            },3000)
+                                                        }
+                                                        else {
+                                                            document.getElementById("user_bet_value").value = "0.05"
+                                                            automatizar();
+                                                        }
+                                                    },3000)
+                                                }
+                                                else {
+                                                    document.getElementById("user_bet_value").value = "0.05"
+                                                    automatizar();
+                                                }
+                                            },3000)
+                                        }
+                                        else {
+                                            document.getElementById("user_bet_value").value = "0.05"
+                                            automatizar();
+                                        }
+                                    },3000)
+                                }
+                                else {
+                                    document.getElementById("user_bet_value").value = "0.05"
+                                    automatizar();
+                                }
+                            },3000)
+                        }
+                        else {
+                            document.getElementById("user_bet_value").value = "0.05"
+                            automatizar();
+                        }
+                    },3000)
+                }
+                else {
+                    document.getElementById("user_bet_value").value = "0.05"
+                    automatizar();
+                }
+            },3000)
+        }
+        else {
+            document.getElementById("user_bet_value").value = "0.05"
+            automatizar();
+        }
+    },3000)
+}
